2a94c37ae6a6470721394fd55a6f094cc6e7d851
jamesvon007 <adolfnapolean@163.com>
2017-09-22 09:10:36 +0800

    FIRST

---
+++ CPR_Test_NOWY/CPR_Framework.h
@@ -0,0 +1,65 @@
+#include <d3dx9.h>
+#include <vector>
+
+//----------------------------------------------------------------------------
+//	Simple class for handling mouse input
+//----------------------------------------------------------------------------
+class Mouse
+{
+public:
+	static D3DXVECTOR2 GetPosition();
+	static bool LeftMouseButton();
+	static bool RightMouseButton();
+};
+
+//----------------------------------------------------------------------------
+//	Simple class for handling keyboard input
+//----------------------------------------------------------------------------
+class Keyboard
+{
+public:
+	enum Key
+	{
+		KEY_UP,
+		KEY_DOWN,
+		KEY_LEFT,
+		KEY_RIGHT,
+		KEY_SPACE,
+		KEY_RETURN,
+		KEY_W,
+		KEY_A,
+		KEY_S,
+		KEY_D
+	};
+
+	static bool IsKeyPressed( Key key );
+};
+
+//----------------------------------------------------------------------------
+//	A class for controlling the camera
+//----------------------------------------------------------------------------
+class Camera
+{
+public:
+	static void LookAt( const D3DXVECTOR3& _eye, const D3DXVECTOR3& _target );
+};
+
+//----------------------------------------------------------------------------
+//	Mesh resource handling class
+//----------------------------------------------------------------------------
+class Mesh
+{
+protected:
+	Mesh();
+
+public:
+	~Mesh();
+
+	static Mesh* LoadFromFile( char filename[] );
+
+	void Render( const D3DXVECTOR3& _position, const D3DXVECTOR3& _rotation, const D3DXVECTOR3& _scale, D3DXVECTOR4 _color );
+
+private:
+	ID3DXMesh*			m_mesh;
+	int					m_numSubsets;
+};
---
+++ CPR_Test_NOWY/CPR_Test.cpp
@@ -0,0 +1,156 @@
+#include "CPR_Framework.h"
+
+Mesh*	g_mesh = 0;
+float	g_angle = 0.0f;
+Mesh*	g_skyscraper = 0;
+
+struct CameraInfo
+{
+	CameraInfo()
+		: mPosition(0.0f, 0.0f, 0.0f),
+		mRight(1.0f, 0.0f, 0.0f),
+		mUp(0.0f, 1.0f, 0.0f),
+		mLook(0.0f, 0.0f, 1.0f),
+		mLastMousePos(0.0f, 0.0f)
+	{
+
+	}
+
+	void Walk(float d)
+	{
+		mPosition += d*mLook;
+	}
+
+	void Strafe(float d)
+	{
+		mPosition += d*mRight;
+	}
+
+	void Pitch(float angle)
+	{
+		// Rotate up and look vector about the right vector.
+		D3DXMATRIX R;
+		D3DXMatrixRotationAxis(&R, &mRight, angle);
+		D3DXVec3TransformNormal(&mUp, &mUp, &R);
+		D3DXVec3TransformNormal(&mLook, &mLook, &R);
+	}
+
+	void RotateY(float angle)
+	{
+		// Rotate the basis vectors about the world y-axis.
+		D3DXMATRIX R;
+		D3DXMatrixRotationY(&R, angle);
+		D3DXVec3TransformNormal(&mRight, &mRight, &R);
+		D3DXVec3TransformNormal(&mUp, &mUp, &R);
+		D3DXVec3TransformNormal(&mLook, &mLook, &R);
+	}
+
+	D3DXVECTOR3 mPosition;
+	D3DXVECTOR3 mLook;
+	D3DXVECTOR3 mRight;
+	D3DXVECTOR3 mUp;
+	D3DXVECTOR2 mLastMousePos;
+};
+
+CameraInfo g_camera;
+
+void UpdateCamera(float _deltaTime);
+
+
+/*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+	This file contains the main loop of the program and works as follows:
+
+		OnInit();
+
+		while( !shouldShutDown )
+		{
+			OnUpdate( deltaTime );
+			OnRender();
+		}
+
+		OnShutdown();
+
+	For vector & matrix math we're using the D3DX Library. 
+	Here are some useful classes & functions (that may or may not be handy):
+
+		D3DXVECTOR3		- x, y, z (floats)
+		D3DXMATRIX		- 16 float values
+		D3DXQUATERNION	- x, y, z, w (floats)
+
+		D3DXVECTOR3*	D3DXVec3Normalize( D3DXVECTOR3 *pOut, const D3DXVECTOR3 *pV );
+		FLOAT			D3DXVec3Length( const D3DXVECTOR3 *pV );
+		FLOAT			D3DXVec3Dot( const D3DXVECTOR3 *pV1, const D3DXVECTOR3 *pV2 );
+		D3DXVECTOR3*	D3DXVec3Cross( D3DXVECTOR3 *pOut, const D3DXVECTOR3 *pV1, const D3DXVECTOR3 *pV2 );
+
+	You can find these and more in the DX SDK documentation.
+
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+
+//----------------------------------------------------------------------------
+void OnInit()
+{
+	// NOTE: there is also unitbox.x, unitsphere.x & unitcylinder.x to use.
+	g_mesh = Mesh::LoadFromFile( "resources/meshes/unitcylinder.x" );
+	g_skyscraper = Mesh::LoadFromFile("resources/meshes/unitbox.x");
+}
+
+//----------------------------------------------------------------------------
+void OnShutdown()
+{
+}
+
+//----------------------------------------------------------------------------
+void OnUpdate( float _deltaTime )
+{
+	UpdateCamera(_deltaTime);
+}
+
+//----------------------------------------------------------------------------
+void OnRender()
+{
+	// render mesh
+	D3DXVECTOR3 pos( 0.0f, 0.0f, 0.0f );
+	D3DXVECTOR3 rot( 0.0f, 0.0f, 0.0f );
+	D3DXVECTOR3 sca( 1.0f, 1.0f, 1.0f );
+	D3DXVECTOR4 color( 1.0f, 0.5f, 0.0f, 1.0f );
+	g_mesh->Render( pos, rot, sca, color );
+
+	g_mesh->Render(pos + D3DXVECTOR3(1.0, 1.0f, 0.0f), rot, sca, color);
+}
+
+void UpdateCamera(float _deltaTime)
+{
+	if (Keyboard::IsKeyPressed(Keyboard::KEY_W))
+	{
+		g_camera.Walk(10.f * _deltaTime);
+	}
+	else if (Keyboard::IsKeyPressed(Keyboard::KEY_S))
+	{
+		g_camera.Walk(-10.f * _deltaTime);
+	}
+	else if (Keyboard::IsKeyPressed(Keyboard::KEY_A))
+	{
+		g_camera.Strafe(-10.f * _deltaTime);
+	}
+	else if (Keyboard::IsKeyPressed(Keyboard::KEY_D))
+	{
+		g_camera.Strafe(10.f * _deltaTime);
+	}
+
+	D3DXVECTOR2 newPos = Mouse::GetPosition();
+	float dx = D3DXToRadian(0.1f*(newPos.x - g_camera.mLastMousePos.x));
+	float dy = D3DXToRadian(0.1f*(newPos.y - g_camera.mLastMousePos.y));
+	g_camera.Pitch(dy);
+	g_camera.RotateY(dx);
+	g_camera.mLastMousePos = newPos;
+
+	D3DXVec3Normalize(&g_camera.mLook, &g_camera.mLook);
+	D3DXVec3Cross(&g_camera.mUp, &g_camera.mLook, &g_camera.mRight);
+	D3DXVec3Normalize(&g_camera.mUp, &g_camera.mUp);
+	D3DXVec3Cross(&g_camera.mRight, &g_camera.mUp, &g_camera.mLook);
+
+	// update camera
+	g_angle += _deltaTime;
+	Camera::LookAt(g_camera.mPosition, g_camera.mPosition + 10.f * g_camera.mLook);
+}
